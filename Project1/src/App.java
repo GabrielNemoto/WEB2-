e20f64fe967ad6d6d83dc0d38c25264f93c8ab9b
GabrielNemoto <gabrielgoncalves1728@gmail.com>
2026-08-05 19:48:47 -0300

    Aula 05/08/2026

---
+++ Project1/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int number1 = 5, number2 = 10, sum = number1 + number2;
+
+        System.out.println("A soma é: " + sum);
+    }
+}
